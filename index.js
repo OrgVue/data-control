9aac91bd11338594d630f4be2eac541efb7500a2
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-05-19 10:34:11 +0100

    Exporting Lens, Const and Identity

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict"
 
 module.exports = {
+  Const: require("./src/Const.js"),
+  Identity: require("./src/Identity.js"),
   IO: require("./src/IO.js"),
+  Lens: require("./src/Lens.js"),
   Task: require("./src/Task.js")
 }
